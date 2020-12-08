4b724542c4d3a84e8820e685f65d52152e19ae7b
Andrea Gray <69823767+andreakatie@users.noreply.github.com>
2020-12-08 03:11:56 -0700

    Basic formatting
    Added a few comments and made edits concerning readability, uniformity, and presentation

--- ConnectFour.cpp
+++ ConnectFour.cpp
@@ -1,19 +1,30 @@
-#include <pthread.h>
+/*
+ *  FILE:           ConnectFour.cpp
+ *  AUTHOR:         Daniel Pietz (/danieljpietz)
+ *  CONTRIBUTORS:   Adam Walker (/the-adam-walker)  --  Debugging and supplemental implementation functions for intended execution of program
+ *                  Andrea Gray (/andreakatie)      --  Formatting
+ *  DESCRIPTION:    <TODO>
+ *  VERSION:        2.4.1                           --  12-08-2020
+ *  NOTES:          Created in partial fulfillment of course requirements for CEC450 (Real-Time Systems) ERAU-PRC
+ *                  Final Project                   --  Exercise 6
+ *                  (C) Daniel J. Pietz. All rights reserved.
+ */
+
 #include <stdio.h>
-#include <iostream>
 #include <sched.h>
-#include <sys/sysinfo.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include "GetBoard.h"
+#include <pthread.h>
 #include <string>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/sysinfo.h>
+#include "GetBoard.h"
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include "connect4/Solver.hpp"
 #include "connect4/Position.hpp"
-#include "connect4/TranspositionTable.hpp"
 #include "connect4/OpeningBook.hpp"
-
+#include "connect4/TranspositionTable.hpp"
 
 cv::VideoCapture cap;
 
@@ -39,7 +50,6 @@ typedef struct {
 
 
 // POSIX thread declarations and scheduling attributes
-//
 pthread_t threads[NUM_THREADS];
 threadParams_t threadParams[NUM_THREADS];
 pthread_attr_t rt_sched_attr[NUM_THREADS];
@@ -51,7 +61,6 @@ pid_t mainpid;
 
 pthread_mutex_t boardLock = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t moveLock = PTHREAD_MUTEX_INITIALIZER;
-
 
 std::array<std::array<char, 7>, 6> capturedBoard;
 std::array<std::array<char, 7>, 6> currentBoard;
@@ -70,73 +79,84 @@ int strongAnswer = 0;
 int weakAnswer = 0;
 
 int tick = 0;
+int moveCount = 0;
 
-/**
-This is the image recognition thread. It constatnly looks at the camera feed
-to determine the state of the board. This board is then returned a 2 dimensional 7x6 array
-of characters with an 'X' denoting positions with a piece and an 'O' denoting empty positions.
-**/
+
+/*
+ *  This is the image recognition thread. It constatnly looks at the camera feed
+ *  to determine the state of the board. This board is then returned a 2 dimensional 7x6 array
+ *  of characters with an 'X' denoting positions with a piece and an 'O' denoting empty positions.
+ */
 
 [[noreturn]] void *thread1(void *args) {
     std::cout << "Image thread running" << std::endl;
 
     capturedBoard = getBoard(cap);
     currentBoard = capturedBoard;
+    
     for (int i = 0; i < boardBufferSize; ++i) {
         boardBuffer.insert(capturedBoard);
     }
+    
     thread1SetupDone = true;
 
     while (true) {
-
-
         if (pthread_mutex_trylock(&boardLock) == 0) {
             capturedBoard = getBoard(cap);
             pthread_mutex_unlock(&boardLock);
         }
-
     }
 }
 
-int moveCount = 0;
 
-/**
-This is the move detection thread. It constantly looks at the capturedBoard object
-and boardBuffer to determine if a move has been played. If a change is detected, 
-the change is added to the buffer. When the buffer is in agreement on  the change,
-it reports that a move has been made.
-*//
+/*
+ *  This is the move detection thread. It constantly looks at the capturedBoard object
+ *  and boardBuffer to determine if a move has been played. If a change is detected, 
+ *  the change is added to the buffer. When the buffer is in agreement on  the change,
+ *  it reports that a move has been made.
+ */
 
 [[noreturn]] void *thread2(void *args) {
     std::cout << "Move thread running" << std::endl;
     thread2SetupDone = true;
+    
     while (true) {
         auto T1 = std::chrono::high_resolution_clock::now();
+        
         if (not isBoardEqual(currentBoard, capturedBoard)) {
+            
             if (pthread_mutex_trylock(&boardLock) == 0) {
                 boardBuffer.insert(capturedBoard);
+                
                 if (isBufferEqual(boardBuffer)) {
                     int moveLoc = getMoveLocation(currentBoard, capturedBoard);
+                    
                     if (moveLoc == 0) {
                         continue;
                     }
+                    
                     auto tMove = std::chrono::high_resolution_clock::now();
                     auto tDelta = std::chrono::duration_cast<std::chrono::milliseconds>(tMove - tLastMove).count();
+                    
                     if (tDelta > (5000 * moveWaitThreshold)) {
                         std::cout << "Move Made! " << moveLoc << std::endl;
                         strongAnswer = 0;
                         weakAnswer = 0;
                         gameString = gameString + std::to_string(moveLoc);
                         std::cout << "STR " << gameString << std::endl;
-                        while (strongAnswer == 0 and std::chrono::duration_cast<std::chrono::milliseconds>(
-                                std::chrono::high_resolution_clock::now() - tMove).count() < 500) {
+                        
+                        while (strongAnswer == 0 and std::chrono::duration_cast<std::chrono::milliseconds>
+                                (std::chrono::high_resolution_clock::now() - tMove).count() < 500) {
                         }
+                        
                         if (strongAnswer != 0) {
                             std::cout << "Next move Strong: " << std::to_string(strongAnswer) << std::endl;
                         }
+                        
                         else {
                             std::cout << "Next move Weak: " << std::to_string(weakAnswer) << std::endl;
                         }
+                        
                         currentBoard = capturedBoard;
                         tLastMove = tMove;
                         std::cout << "CURRENT\n";
@@ -144,32 +164,32 @@ it reports that a move has been made.
                         std::cout << "Captured\n";
                         printBoard(capturedBoard);
                     }
+                    
+                    auto T2 = std::chrono::high_resolution_clock::now();
                     //std::cout << "CURRENT\n";
                     //printBoard(currentBoard);
                     //std::cout << "Captured\n";
                     //printBoard(capturedBoard);
-                    auto T2 = std::chrono::high_resolution_clock::now();
                     //std::cout << "TIME: " << std::chrono::duration_cast<std::chrono::nanoseconds>(T2 - T1).count() << std::endl;
-
                 }
+                
+                // Release shared resource
                 pthread_mutex_unlock(&boardLock);
-            }
-        }
-
-    }
+                
+            } //END if-equal buffer comparison
+        } //END if locking shared resource
+    } //END if-equal board comparison
 }
 
-/**
-Strong solver thread. This will return the best possible move the player can make, using number of remaining moves
-needed to win as a metric. This takes several several seconds to solve early in the game, so the weak solution is given
-if it takes more than half a second to respond.
-**/
+/*
+ *  Strong solver thread. This will return the best possible move the player can make, using number of remaining moves
+ *  needed to win as a metric. This takes several several seconds to solve early in the game, so the weak solution is given
+ *  if it takes more than half a second to respond.
+ */
 
 [[noreturn]] void *thread3(void *args) {
     std::cout << "Strong solver thread running" << std::endl;
-
     GameSolver::Connect4::Solver solver;
-
     bool weak = false;
     std::string opening_book = "connect4/7x6.book";
     solver.loadBook(opening_book);
@@ -178,83 +198,97 @@ if it takes more than half a second to respond.
     thread3SetupDone = true;
 
     while (true) {
+        
         if (not(previousGameString == gameString)) {
             //if (pthread_mutex_trylock(&moveLock) == 0) {
+            
             for (int column = 1; column <= 7; column++) {
                 std::string copyGameString = gameString;
                 copyGameString.std::string::append(std::to_string(column));
                 //std::cout << "T1 " << P.play(copyGameString) << " T2 " << copyGameString.size() << std::endl;
+                
                 if (P.play(copyGameString) != copyGameString.size()) {
                     //std::cerr << "Next Move: " << copyGameString << ": Invalid move " << (P.nbMoves() + 1) << " \""
                               //<< gameString << "\"" << std::endl;
-                } else {
+                } 
+                
+                else {
                     solver.reset();
                     int score = solver.solve(P, weak);
                     strongAnswer = column;
                     //std::cout << "Next Move (" << column << "): " << copyGameString << " score: " << score
                               //<< " nodes iterated: " << solver.getNodeCount();
                 }
+                
                 std::cout << std::endl;
             }
+            
             previousGameString = gameString;
             //pthread_mutex_unlock(&moveLock);
             //}
-        }
-    }
-
+            
+        } //END if-equal game strings
+    } //END infinite while loop
 }
 
-/**
-Weak solver thread. This solution executes extremely quickly but only guarenteess 
-that the opponent will not win on the next move. It is used as a reccomendation if the 
-strong solver takes more than half a second to return a result.
-**/
+
+/*
+ *  Weak solver thread. This solution executes extremely quickly but only guarenteess 
+ *  that the opponent will not win on the next move. It is used as a reccomendation if the 
+ *  strong solver takes more than half a second to return a result.
+ */
 
 [[noreturn]] void *thread4(void *args) {
     std::cout << "Weak solver thread running" << std::endl;
-
     GameSolver::Connect4::Position P;
     std::string previousGameString = "";
     std::srand(std::time(nullptr));
     thread4SetupDone = true;
 
     while (true) {
+        
         if (not(previousGameString == gameString)) {
             weakAnswer = 0;
             auto T1 = std::chrono::high_resolution_clock::now();
+            
             for (int i = 1; i <= 24; i++) {
                 int column = 1 + std::rand()/((RAND_MAX + 1u)/7);
                 P.play(gameString);
+                
                 if (P.canPlay(column)) {
                     weakAnswer = column;
                     break;
                 }
             }
+            
             if (weakAnswer == 0) {
+                
                 for (int i = 1; i <= 7; i++) {
                     int column = i;
                     P.play(gameString);
+                    
                     if (P.canPlay(column)) {
                         weakAnswer = column;
                         break;
                     }
                 }
+                
                 if (weakAnswer == 0) {
                     throw;
                 }
             }
+            
             previousGameString = gameString;
             auto T2 = std::chrono::high_resolution_clock::now();
             std::cout << "TIME: " << std::chrono::duration_cast<std::chrono::nanoseconds>(T2 - T1).count() << std::endl;
-        }
-
-    }
-
+            
+        } //END if-equal game strings
+    } //END inifinite while 
 }
+
 
 void print_scheduler(void) {
     int schedType, scope;
-
     schedType = sched_getscheduler(getpid());
 
     switch (schedType) {
@@ -279,12 +313,12 @@ void print_scheduler(void) {
         printf("PTHREAD SCOPE PROCESS\n");
     else
         printf("PTHREAD SCOPE UNKNOWN\n");
-
 }
 
-/**
-This function initializes all other threads, done in a SCHED_FIFO scheduling system
-**/
+
+/*
+ *  This function initializes all other threads, done in a SCHED_FIFO scheduling system
+ */
 
 int main(int argc, char *argv[]) {
     std::cout << "Welcome to Connect 4" << std::endl;
@@ -295,17 +329,14 @@ int main(int argc, char *argv[]) {
     int i;
     cpu_set_t threadcpu;
     int coreid;
-
     numberOfProcessors = get_nprocs_conf();
 
     printf("This system has %d processors with %d available\n", numberOfProcessors, get_nprocs());
     printf("The test thread created will be run on a specific core based on thread index\n");
 
     mainpid = getpid();
-
     rt_max_prio = sched_get_priority_max(SCHED_FIFO);
     rt_min_prio = sched_get_priority_min(SCHED_FIFO);
-
     print_scheduler();
     rc = sched_getparam(mainpid, &main_param);
     main_param.sched_priority = rt_max_prio;
@@ -316,31 +347,23 @@ int main(int argc, char *argv[]) {
     }
 
     print_scheduler();
-
-
+    
     printf("rt_max_prio=%d\n", rt_max_prio);
     printf("rt_min_prio=%d\n", rt_min_prio);
 
-
-
-    ///////
-    //////
     i = 0;
     CPU_ZERO(&threadcpu);
-
     coreid = i % numberOfProcessors;
+    
     printf("Setting thread %d to core %d\n", i, coreid);
 
     CPU_SET(coreid, &threadcpu);
-
     rc = pthread_attr_init(&rt_sched_attr[i]);
     rc = pthread_attr_setinheritsched(&rt_sched_attr[i], PTHREAD_EXPLICIT_SCHED);
     rc = pthread_attr_setschedpolicy(&rt_sched_attr[i], MY_SCHEDULER);
     rc = pthread_attr_setaffinity_np(&rt_sched_attr[i], sizeof(cpu_set_t), &threadcpu);
-
     rt_param[i].sched_priority = rt_max_prio - i - 1;
     pthread_attr_setschedparam(&rt_sched_attr[i], &rt_param[i]);
-
     threadParams[i].threadIdx = i;
 
     pthread_create(&threads[i],               // pointer to thread descriptor
@@ -348,28 +371,25 @@ int main(int argc, char *argv[]) {
                    thread1,              // thread function entry point
                    (void *) &(threadParams[i]) // parameters to pass in
     );
+    
     while (!thread1SetupDone) {
-
     }
+    
     std::cout << "Thread 1 Setup Done" << std::endl;
-    //////
-    //////
+
     i = 1;
     CPU_ZERO(&threadcpu);
-
     coreid = i % numberOfProcessors;
+    
     printf("Setting thread %d to core %d\n", i, coreid);
 
     CPU_SET(coreid, &threadcpu);
-
     rc = pthread_attr_init(&rt_sched_attr[i]);
     rc = pthread_attr_setinheritsched(&rt_sched_attr[i], PTHREAD_EXPLICIT_SCHED);
     rc = pthread_attr_setschedpolicy(&rt_sched_attr[i], MY_SCHEDULER);
     rc = pthread_attr_setaffinity_np(&rt_sched_attr[i], sizeof(cpu_set_t), &threadcpu);
-
     rt_param[i].sched_priority = rt_max_prio - i - 1;
     pthread_attr_setschedparam(&rt_sched_attr[i], &rt_param[i]);
-
     threadParams[i].threadIdx = i;
 
     pthread_create(&threads[i],               // pointer to thread descriptor
@@ -378,25 +398,19 @@ int main(int argc, char *argv[]) {
                    (void *) &(threadParams[i]) // parameters to pass in
     );
 
-
-    //////
-    //////
     i = 2;
     CPU_ZERO(&threadcpu);
-
     coreid = i % numberOfProcessors;
+    
     printf("Setting thread %d to core %d\n", i, coreid);
 
     CPU_SET(coreid, &threadcpu);
-
     rc = pthread_attr_init(&rt_sched_attr[i]);
     rc = pthread_attr_setinheritsched(&rt_sched_attr[i], PTHREAD_EXPLICIT_SCHED);
     rc = pthread_attr_setschedpolicy(&rt_sched_attr[i], MY_SCHEDULER);
     rc = pthread_attr_setaffinity_np(&rt_sched_attr[i], sizeof(cpu_set_t), &threadcpu);
-
     rt_param[i].sched_priority = rt_max_prio - i - 1;
     pthread_attr_setschedparam(&rt_sched_attr[i], &rt_param[i]);
-
     threadParams[i].threadIdx = i;
 
     pthread_create(&threads[i],               // pointer to thread descriptor
@@ -406,29 +420,23 @@ int main(int argc, char *argv[]) {
     );
 
     while (!thread3SetupDone) {
-
     }
+    
     std::cout << "Thread 3 Setup Done" << std::endl;
-
-    //////
-    //////
 
     i = 3;
     CPU_ZERO(&threadcpu);
-
     coreid = i % numberOfProcessors;
+    
     printf("Setting thread %d to core %d\n", i, coreid);
 
     CPU_SET(coreid, &threadcpu);
-
     rc = pthread_attr_init(&rt_sched_attr[i]);
     rc = pthread_attr_setinheritsched(&rt_sched_attr[i], PTHREAD_EXPLICIT_SCHED);
     rc = pthread_attr_setschedpolicy(&rt_sched_attr[i], MY_SCHEDULER);
     rc = pthread_attr_setaffinity_np(&rt_sched_attr[i], sizeof(cpu_set_t), &threadcpu);
-
     rt_param[i].sched_priority = rt_max_prio - i - 1;
     pthread_attr_setschedparam(&rt_sched_attr[i], &rt_param[i]);
-
     threadParams[i].threadIdx = i;
 
     pthread_create(&threads[i],               // pointer to thread descriptor
@@ -436,14 +444,11 @@ int main(int argc, char *argv[]) {
                    thread4,              // thread function entry point
                    (void *) &(threadParams[i]) // parameters to pass in
     );
-    //////
-    //////
 
     while (!thread4SetupDone) {
-
     }
+    
     std::cout << "Thread 4 Setup Done\nAll Threads Running\n" << std::endl;
-
     std::cout << "\033[1;31mWELCOME TO CONNECT 4!\033[0m\n" << std::endl;
 
     for (i = 0; i < NUM_THREADS; i++)
